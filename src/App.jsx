539b32de924f34c763e11070d13ada6ac86a485d
TzuChien Chang <25aw0123@jec.ac.jp>
2026-01-23 20:07:44 +0900

    レイアウト構成を追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Layout from './Layout'
 
 function App() {
 
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/" element={<AboutPage />} />
+          <Route path="/works/:id" element={<WorkDetailPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
