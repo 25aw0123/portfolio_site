c4351858a653794b205283524b9fd3600258d064
TzuChien Chang <25aw0123@jec.ac.jp>
2026-01-23 20:50:58 +0900

    ヘッダーにナビゲーションを追加

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+    </nav>
   )
 }
 
