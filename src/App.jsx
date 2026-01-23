174680fa613f43ed770ae403c3eb2e9e537ae739
TzuChien Chang <25aw0123@jec.ac.jp>
2026-01-23 20:37:02 +0900

    レイアウト構成を追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Layout from './Layout'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import WorkDetailPage from './pages/WorkDetailPage'
 
 function App() {
 
@@ -9,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="/" element={<AboutPage />} />
+          <Route path="/about" element={<AboutPage />} />
           <Route path="/works/:id" element={<WorkDetailPage />} />
         </Route>
       </Routes>
---
+++ src/worksData.js
@@ -0,0 +1,10 @@
+export const worksData = [
+{
+  id: 1,
+  title:'',
+  category:'',
+  description:'',
+  tech:'',
+  img:''
+}
+];
