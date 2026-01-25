5ae00d3ee9c608fd3499e3bd4589c52abfce10e9
TzuChien Chang <25aw0123@jec.ac.jp>
2026-01-26 01:57:12 +0900

    ヘッダーを作成

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,44 @@
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import styles from './Header.module.css'
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-    </nav>
+    <header className={styles.header}>
+      <div className={`${styles.menu} ${isOpen ? styles.menuOpen : ''}`}>
+        <button
+          type="button"
+          className={`${styles.hamburgerButton} ${isOpen ? styles.hamburgerOpen : ''}`}
+          onClick={toggleMenu}
+          aria-label="メニュー切り替え"
+        >
+          <span className={styles.hamburgerLine}></span>
+          <span className={styles.hamburgerLine}></span>
+        </button>
+      </div>
+
+      <nav className={`${styles.hamburgerNav} ${isOpen ? styles.navOpen : ''}`}>
+        <ul className={styles.navLinks}>
+          <li onClick={toggleMenu}>
+            <Link to="/">
+              <span className={styles.jaText}>ホーム</span>
+              <span className={styles.enText}>home</span>
+            </Link>
+          </li>
+          <li onClick={toggleMenu}>
+            <Link to="/about">
+              <span className={styles.jaText}>私のこと</span>
+              <span className={styles.enText}>about</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
