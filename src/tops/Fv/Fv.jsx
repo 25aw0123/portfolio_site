a1f6ed52f694b5c25ed1d57299ec75241ecdd0e3
TzuChien Chang <25aw0123@jec.ac.jp>
2026-01-27 10:44:28 +0900

    ファストビューを修正

--- src/tops/Fv/Fv.jsx
+++ src/tops/Fv/Fv.jsx
@@ -3,12 +3,13 @@ import styles from './Fv.module.css';
 const Fv = () => {
   return (
     <section className={styles.firstView}>
+      <div className={`${styles.bgPattern} ${styles.paper}`}></div>
 
       <div className={styles.titleArea}>
         <h1><img src="/images/headline.png" alt="ちょう's portfolio" width="361" className={styles.headline} /></h1>
         <h2><img src='/images/subheading.png' alt='front-end engineer' width='222' className={styles.subheading} /></h2>
         <div className={styles.subTitle}>
-          <img src='/images/subheading2.png' alt='どんな状況でも迷わせないUI' width='645' className={styles.subheading2} />
+          <img src='/images/subheading2.png' alt='使う人の行動を考え、迷わず使えるカタチをつくる。' width='442' className={styles.subheading2} />
         </div>
       </div>
 
